a4c15b9658ff1f92bb56da7f4f1125468ed86045
DilonH <dilonhewamanna@outlook.com>
2024-04-30 13:20:03 +1000

    first effort at combining obj detect and vo

--- LK_mono_vis_odometry.py
+++ LK_mono_vis_odometry.py
@@ -6,9 +6,6 @@ from lib.visualization import plotting
 
 class VisualOdometry():
     def __init__(self, calib_dir):
-        self.detector = cv2.FastFeatureDetector.create(threshold = 25, nonmaxSuppression=True)
-        self.kMinNumFeature = 1500
-
         with open(calib_dir + "/camera_matrix.pkl", 'rb') as f:
             self.K = pickle.load(f)
 
@@ -18,17 +15,11 @@ class VisualOdometry():
 
         self.lk_params = dict(winSize  = (21, 21), 
              	criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
-
-    def process_first_frame(self, frame):
-        keypoints_prev = self.detector.detect(frame)
-        # keypoint detectors inherit the FeatureDetector interface
-        return np.array([i.pt for i in keypoints_prev], dtype=np.float32)
     
-    def process_frame(self, old_frame, new_frame, prev_keys):
+    def process_frame(self, old_frame, new_frame, prev_keys, new_keys):
         k1, k2 = self.optical_flow(old_frame, new_frame, prev_keys)
         T = self.get_state(k1, k2)
 
-        new_keys = self.detector.detect(new_frame)
         return np.array([i.pt for i in new_keys], dtype=np.float32), T
 
     def optical_flow(self, prev_img, curr_img, prev_keys):
@@ -41,15 +32,96 @@ class VisualOdometry():
         k2 = k2[status == 1]
         return k1, k2
     
+    @staticmethod
+    def _form_transf(R, t):
+        """
+        Makes a transformation matrix from the given rotation matrix and translation vector
+
+        Parameters
+        ----------
+        R (ndarray): The rotation matrix
+        t (list): The translation vector
+
+        Returns
+        -------
+        T (ndarray): The transformation matrix
+        """
+        T = np.eye(4, dtype=np.float64)
+        T[:3, :3] = R
+        T[:3, 3] = t
+        return T
+    
     def get_state(self, k1, k2):
         E, _ = cv2.findEssentialMat(k2, k1, self.K, threshold=1, method = cv2.RANSAC)
         n, R, t, mask = cv2.recoverPose(E, k1, k2)
         t = np.squeeze(t)
 
-        T = np.eye(4, dtype=np.float64)
-        T[:3, :3] = R
-        T[:3, 3] = t
-        return T
+        R, t = self.decomp_essential_mat(E, k1, k2)
+
+        transformation_matrix = self._form_transf(R, np.squeeze(t))
+        return transformation_matrix
+    
+def decomp_essential_mat(self, E, q1, q2):
+        """
+        Decompose the Essential matrix
+
+        Parameters
+        ----------
+        E (ndarray): Essential matrix
+        q1 (ndarray): The good keypoints matches position in i-1'th image
+        q2 (ndarray): The good keypoints matches position in i'th image
+
+        Returns
+        -------
+        right_pair (list): Contains the rotation matrix and translation vector
+        """
+        def sum_z_cal_relative_scale(R, t):
+            # Get the transformation matrix
+            T = self._form_transf(R, t)
+            # Make the projection matrix
+            P = np.matmul(np.concatenate((self.K, np.zeros((3, 1))), axis=1), T)
+
+            # Triangulate the 3D points
+            hom_Q1 = cv2.triangulatePoints(self.P, P, q1.T, q2.T)
+            # Also seen from cam 2
+            hom_Q2 = np.matmul(T, hom_Q1)
+
+            # Un-homogenize
+            uhom_Q1 = hom_Q1[:3, :] / hom_Q1[3, :]
+            uhom_Q2 = hom_Q2[:3, :] / hom_Q2[3, :]
+
+            # Find the number of points there has positive z coordinate in both cameras
+            sum_of_pos_z_Q1 = sum(uhom_Q1[2, :] > 0)
+            sum_of_pos_z_Q2 = sum(uhom_Q2[2, :] > 0)
+
+            # Form point pairs and calculate the relative scale
+            relative_scale = np.mean(np.linalg.norm(uhom_Q1.T[:-1] - uhom_Q1.T[1:], axis=-1)/
+                                     np.linalg.norm(uhom_Q2.T[:-1] - uhom_Q2.T[1:], axis=-1))
+            return sum_of_pos_z_Q1 + sum_of_pos_z_Q2, relative_scale
+
+        # Decompose the essential matrix
+        R1, R2, t = cv2.decomposeEssentialMat(E)
+        t = np.squeeze(t)
+
+        # Make a list of the different possible pairs
+        pairs = [[R1, t], [R1, -t], [R2, t], [R2, -t]]
+
+        # Check which solution there is the right one
+        z_sums = []
+        relative_scales = []
+        for R, t in pairs:
+            z_sum, scale = sum_z_cal_relative_scale(R, t)
+            z_sums.append(z_sum)
+            relative_scales.append(scale)
+
+        # Select the pair there has the most points with positive z coordinate
+        right_pair_idx = np.argmax(z_sums)
+        right_pair = pairs[right_pair_idx]
+        relative_scale = relative_scales[right_pair_idx]
+        R1, t = right_pair
+        t = t * relative_scale
+
+        return [R1, t]
 
 def main():
     data_dir = "camera_params"
--- main_video_test.py
+++ main_video_test.py
@@ -1,80 +1,84 @@
 import cv2 as cv
 import numpy as np
+from lib.visualization import plotting
+import pickle
+from matplotlib import pyplot as plt
 
 from image_read import image_read
 from colour_filter import colour_filter
 from get_contours import get_contours
 from contour_filters import contour_filter_4
+from LK_mono_vis_odometry import VisualOdometry
 
-# Colours are in HSV
+def edge_detection(frame):
+    # Colours are in HSV
+    black_min = (0, 0, 0)
+    black_max = (255, 255, 50)
 
-black_min = (0, 0, 0)
-black_max = (255, 255, 50)
-
-capture = cv.VideoCapture(0)
-capture.set(3, 128)
-capture.set(4, 96)
-
-while True:
-
-    retval, frame = capture.read() 
-
-    if not retval:
-        break
-    
     blank_frame = np.zeros_like(frame)
-    # blank_frame = cv.copyMakeBorder(blank_frame, 1, 1, 1, 1, cv.BORDER_CONSTANT)
-    # blank_frame = frame
-
-
-    frame_resize = frame
-    frame_blur = frame_resize
-    # frame_blur = cv.GaussianBlur(frame, (45,45), 1)
-    # frame_yellow = colour_filter(frame_blur, yellow_min, yellow_max, 5, 5, 0)
-    # frame_blue = colour_filter(frame_blur, blue_min, blue_max, 5, 5, 0)
-    frame_black = colour_filter(frame_blur, black_min, black_max, 5, 5, 0)
-
-    # contours_yellow = get_contours(frame_yellow)
-    # contours_blue = get_contours(frame_blue)
+    frame_black = colour_filter(frame, black_min, black_max, 5, 5, 0)
     contours_black = get_contours(frame_black)
-    # contours_yellow, yellow_track = contour_filter_4(contours_yellow)
-    # contours_blue, blue_track = contour_filter_4(contours_blue)
-
-    # cv.drawContours(blank_frame, contours_yellow, -1, yellow_max)
-    # cv.drawContours(blank_frame, contours_blue, -1, (255, 0, 0))
-    # cv.drawContours(frame, contours_yellow, -1, (255,0,0))
-    # cv.drawContours(frame, contours_blue, -1, yellow_max)
     cv.drawContours(frame, contours_black, -1, (0,0,255))
-
-    # try:
-    #         # cv.line(blank_frame, 
-    #         #         np.intp((yellow_track[0] - 100 * np.cos(yellow_track[2]), yellow_track[1] + 100 * np.sin(yellow_track[2]))),
-    #         #         np.intp((yellow_track[0] + 100 * np.cos(yellow_track[2]), yellow_track[1] - 100 * np.sin(yellow_track[2]))),
-    #         #         yellow_max, 3)
-    #         # cv.line(blank_frame, 
-    #         #         np.intp((blue_track[0] - 100 * np.cos(blue_track[2]), blue_track[1] + 100 * np.sin(blue_track[2]))),
-    #         #         np.intp((blue_track[0] + 100 * np.cos(blue_track[2]), blue_track[1] - 100 * np.sin(blue_track[2]))),
-    #         #         (255, 0, 0), 3)
-            
-    #         cv.line(frame, 
-    #                 np.intp((yellow_track[0] - 100 * np.cos(yellow_track[2]), yellow_track[1] + 100 * np.sin(yellow_track[2]))),
-    #                 np.intp((yellow_track[0] + 100 * np.cos(yellow_track[2]), yellow_track[1] - 100 * np.sin(yellow_track[2]))),
-    #                 (255,0,0), 3)
-    #         cv.line(frame, 
-    #                 np.intp((blue_track[0] - 100 * np.cos(blue_track[2]), blue_track[1] + 100 * np.sin(blue_track[2]))),
-    #                 np.intp((blue_track[0] + 100 * np.cos(blue_track[2]), blue_track[1] - 100 * np.sin(blue_track[2]))),
-    #                 yellow_max, 3)
-    # except:
-    #         None
-
     cv.imshow('frame', frame)
     key = cv.waitKey(1)
+    return contours_black
 
-    if key == ord('f'):
-        break
 
-    elif key == ord('p'):
-        cv.waitKey(-1)
 
-    # elif key == ord('r'):
-    #     capture = cv.VideoCapture(video)
+def main():
+
+
+    estimated_path = []
+    start_pose =  np.ones((3, 4))
+    start_translation = np.zeros((3,1))
+    start_rotation = np.identity(3)
+    start_pose = np.concatenate((start_rotation, start_translation), axis = 1)
+
+
+
+    capture = cv.VideoCapture(0)   
+    capture.set(3, 128)
+    capture.set(4, 96)
+    
+    data_dir = "camera_params"
+    vo = VisualOdometry(data_dir)
+
+    old_frame = None
+    new_frame = None
+
+    curr_pose = start_pose
+
+    prev_keys = 0
+
+    traj = np.zeros((600,600,3), dtype=np.uint8)
+
+    i = 0
+
+    while(capture.isOpened()):
+
+        if(i == 0):
+            ret, new_frame = capture.read()
+            prev_keys = edge_detection(new_frame)
+            old_frame = new_frame
+            i += 1
+            continue
+
+        ret, new_frame = capture.read()
+        new_keys = edge_detection(new_frame)
+        prev_keys, transf = vo.process_frame(old_frame, new_frame, prev_keys, new_keys)
+        old_frame = new_frame
+
+        curr_pose = np.matmul(curr_pose, np.linalg.inv(transf))
+        estimated_path.append((curr_pose[0, 3], curr_pose[2, 3]))
+         
+        if (i == 500):
+            break
+
+        i += 1
+
+    cv.destroyAllWindows() 
+
+    plotting.visualize_paths(estimated_path, estimated_path, "Visual Odometry", file_out="blah.html")
+
+if __name__ == "__main__":
+    main()
