46aad72f8edb66c821ba4786ae013569511a2357
raym-t <raymond.trinh44@gmail.com>
2024-04-30 16:00:37 +1000

    Update blink_led.py

--- blink_led.py
+++ blink_led.py
@@ -1,9 +1,16 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+import RPi.GPIO as GPIO         # Import Raspberry Pi GPIO library
+from time import sleep          # Import the sleep function 
 
-p = GPIO.PWM(12, 0.5)
-p.start(50)
-input('Press return to stop:')   # use raw_input for Python 2
-p.stop()
-GPIO.cleanup()
+pinLED = 4                      # LED GPIO Pin
+
+GPIO.setmode(GPIO.BCM)          # Use GPIO pin number
+GPIO.setwarnings(False)         # Ignore warnings in our case
+GPIO.setup(pinLED, GPIO.OUT)    # GPIO pin as output pin
+
+while True:                          # Endless Loop
+    GPIO.output(pinLED, GPIO.HIGH)   # Turn on
+    print("LED on")                    # Prints state to console
+    sleep(1)                         # Pause 1 second
+    GPIO.output(pinLED, GPIO.LOW)    # Turn off
+    print("LED off")                   # Prints state to console
+    sleep(1)                         # Pause 1 second
