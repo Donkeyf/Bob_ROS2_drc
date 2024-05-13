f66f92aec8e3db96b7c288eb9e002b60cef02884
DilonH <dilonhewamanna@outlook.com>
2024-05-13 15:48:15 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,141 +1,78 @@
 import cv2 as cv
 import numpy as np
-from monocular_VO import VisualOdometry
+
+from image_read import image_read
+from colour_filter import colour_filter
+from get_contours import get_contours
+from contour_filters import contour_filter_4
 
 from midline_coords import midline_coords
 from pinehsv_to_cvhsv import pinehsv_to_cvhsv
-from colour_filter import colour_filter
 
-import csv
+from motor_control import MotorControl
 
-def format_points(p1):
-    p1_3D = np.ones((3, p1.shape[0]))
-    p1_3D[0], p1_3D[1] = p1[:, 0].copy(), p1[:, 1].copy()
-    return p1_3D
+pine_blue_min = (190, 30, 30)
+pine_blue_max = (215, 100, 100)
+yellow_min = (20, 100, 100)
+yellow_max = (30, 255, 255)
 
-def line_detection(frame):
-    # Colours are in HSV
+blue_min = pinehsv_to_cvhsv(pine_blue_min)
+blue_max = pinehsv_to_cvhsv(pine_blue_max)
 
-    # Range is 0-180, 0-255, 0-255
-    yellow_min = (20, 100, 100)
-    yellow_max = (30, 255, 255)
+capture = cv.VideoCapture(0)
+capture.set(3, 320)
+capture.set(4, 240)
 
-    # Colour for dilon webcam
+mc = MotorControl()
+mc.forward()
 
-    pine_blue_min = (190, 30, 70)
-    pine_blue_max = (205, 100, 100)
+while True:
+    mc.change_speed(100)
 
-    blue_min = pinehsv_to_cvhsv(pine_blue_min)
-    blue_max = pinehsv_to_cvhsv(pine_blue_max)
+    retval, frame = capture.read() 
 
-    frame_blue = colour_filter(frame, blue_min, blue_max, 5, 5, 0)
-    frame_yellow = colour_filter(frame, yellow_min, yellow_max, 5, 5, 0)
+    if not retval:
+        break
+    
+    blank_frame = np.zeros_like(frame)
 
-    blue_coords = midline_coords(frame_blue)
-    yellow_coords = midline_coords(frame_yellow)
+    frame_resize = frame
+    frame_blur = frame_resize
+    frame_blue = colour_filter(frame_blur, blue_min, blue_max, 5, 5, 0)
+    frame_yellow = colour_filter(frame_blur, yellow_min, yellow_max, 5, 5, 0)
 
-    return blue_coords
+    # Colour is in BGR
 
+    blue_coords, blue_angle, blue_x = midline_coords(frame_blue)
+    if len(blue_coords) == 0:
+        True
+    else:
+        for point1, point2 in zip(blue_coords, blue_coords[1:]): 
+            cv.line(frame, tuple(np.intp(point1)), tuple(np.intp(point2)), [0, 255, 255], 2) 
 
+    yellow_coords, yellow_angle, yellow_x = midline_coords(frame_yellow)
+    if len(yellow_coords) == 0:
+        True
+    else:
+        for point1, point2 in zip(yellow_coords, yellow_coords[1:]): 
+            cv.line(frame, tuple(np.intp(point1)), tuple(np.intp(point2)), [255, 0, 0], 2) 
+    
+    if (blue_angle != None) & (yellow_angle != None):
+        angle = (blue_angle + yellow_angle) / 2
+    elif (blue_angle == None) & (yellow_angle != None):
+        angle = yellow_angle
+    elif (blue_angle != None) & (yellow_angle == None):
+        angle = blue_angle
+    else:
+        angle = 0
 
-
-
-def main():
-    data_dir = "calibration"
-    vo = VisualOdometry(data_dir)
-
-    start_pose =  np.ones((3, 4))
-    start_translation = np.zeros((3,1))
-    start_rotation = np.identity(3)
-    start_pose = np.concatenate((start_rotation, start_translation), axis = 1)
-    curr_pose = start_pose
-
-    bstart_pose =  np.ones((3, 4))
-    bstart_translation = np.zeros((3,1))
-    bstart_rotation = np.identity(3)
-    bstart_pose = np.concatenate((bstart_rotation, bstart_translation), axis = 1)
-    bcurr_pose = bstart_pose
-
-    ystart_pose =  np.ones((3, 4))
-    ystart_translation = np.zeros((3,1))
-    ystart_rotation = np.identity(3)
-    ystart_pose = np.concatenate((ystart_rotation, ystart_translation), axis = 1)
-    ycurr_pose = ystart_pose
-
-    capture = cv.VideoCapture(0)   
-    capture.set(3, 128)
-    capture.set(4, 96)
-
-    old_frame = None
-    new_frame = None
-
-    bot_path = []
-    b_pose = []
-    y_pose = []
-
-    prev_keys = 0
-
-    b_pts_prev = []
-    y_pts_prev = []
-
-    mtx1 = 0
-
-
-    i = 0
-    key = -1
-    while(capture.isOpened()):
-
-        if(i == 0):
-            ret, new_frame = capture.read()
-            b_pts_prev = line_detection(new_frame)
-            mtx1 = vo.P
-            prev_keys = vo.process_first_frame(new_frame)
-            old_frame = new_frame
-            i += 1
-            continue
-
-        ret, new_frame = capture.read()
-        prev_keys, transf, mtx2 = vo.process_frame(old_frame, new_frame, prev_keys)
-        old_frame = new_frame
-
-        curr_pose = np.matmul(curr_pose, np.linalg.inv(transf))
-        bot_path.append((curr_pose[0, 3], 0, curr_pose[2, 3]))
-
-        b_cur_pts = line_detection(new_frame)
-        
-
-        b_points_3D = vo.triangulate(mtx1, mtx2, b_pts_prev, b_cur_pts)
-        mtx1 = mtx2
-        
-        b_pts_prev = b_cur_pts
-        # y_pts_prev = y_cur_pts
-
-        T_b = vo.pnp(b_points_3D, b_cur_pts)
-        
-
-
-        bcurr_pose = np.matmul(bcurr_pose, np.linalg.inv(b_points_3D))
-        b_pose.append(bcurr_pose)
-
-
-        # ycurr_pose = np.matmul(ycurr_pose, np.linalg.inv(y_points_3D))
-        # y_pose.append(ycurr_pose)
-
-        i += 1
-
-        key = cv.waitKey(1)
-        
-        if (key > -1):
-            break
-
-    f = open("line_poses.csv", "w")
-    writer = csv.writer(f)
-
-    for x in len(bcurr_pose):
-        row = [bcurr_pose[x][0], bcurr_pose[x][2]]
-        writer.writerow(row)
-
-    f.close()
-
-if __name__=="__main__":
-    main()
+    if (140 < blue_x < 160):
+        mc.course_correction(True)
+    elif(160 < yellow_x < 180):
+        mc.course_correction(False)
+    elif(angle < -10):
+        mc.turn_left(angle)
+    elif(angle > 10):
+        mc.turn_right(angle)
+    else:
+        mc.change_speed(100)
