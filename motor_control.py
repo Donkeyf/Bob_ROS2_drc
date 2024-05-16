c42d99068e1c7648c3b8e3ce4e889a0d99c0a2db
DilonH <dilonhewamanna@outlook.com>
2024-05-16 19:06:50 +1000

    Update motor_control.py

--- motor_control.py
+++ motor_control.py
@@ -79,7 +79,7 @@ class MotorControl():
 
 
     def turn_right(self, angle):
-        factor =  default_speed * 0.3
+        factor =  default_speed * 0.8
         if angle < 45:
             speed = (1 - (-angle / 90)) * factor
         else:
@@ -93,7 +93,7 @@ class MotorControl():
 
 
     def turn_left(self, angle):
-        factor =  default_speed * 0.3
+        factor =  default_speed * 0.8
         if angle < 45:
             speed = (1 - (angle / 90)) * factor
         else:
