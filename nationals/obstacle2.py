ddf7854bd80d35fbd6d7aed46e61d0f93d56d979
DilonH <dilonhewamanna@outlook.com>
2024-07-07 23:47:50 +1000

    Update obstacle2.py

--- nationals/obstacle2.py
+++ nationals/obstacle2.py
@@ -1,10 +1,21 @@
 import cv2
 import numpy as np
+from colour_filter import colour_filter
+from pinehsv_to_cvhsv import pinehsv_to_cvhsv
 
+pine_purple_min = (270, 10, 25)
+pine_purple_max = (330, 60, 100)
+
+purple_min = pinehsv_to_cvhsv(pine_purple_min)
+purple_max = pinehsv_to_cvhsv(pine_purple_max)
 
 def detect_rectangles(image_path):
     image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    frame_blur = cv2.GaussianBlur(image, (11,11), 100)
+
+    purple = colour_filter(frame_blur, purple_min, purple_max)
+    gray = cv2.cvtColor(purple, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
     # Debug: Show the result of edge detection
