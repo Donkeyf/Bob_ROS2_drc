5dacc23467735f8343094f3cf5850f27440c31da
raym-t <raymond.trinh44@gmail.com>
2024-07-08 21:26:43 +1000

    Update main5.py

--- nationals/main5.py
+++ nationals/main5.py
@@ -157,14 +157,14 @@ try:
             finish_angle = find_finish(frame_blur)
 
         
-        if (angle != None):
-            angle = angle * OBJECT_MULTIPLIER
-            left_motor, right_motor, previous_time, previous_error, current_integral \
-                = pid_motor_speed(DEFAULT_SPEED, angle, 0, previous_time, previous_error, current_integral, KP, KI, KD)
+        # if (angle != None):
+        #     angle = angle * OBJECT_MULTIPLIER
+        #     left_motor, right_motor, previous_time, previous_error, current_integral \
+        #         = pid_motor_speed(DEFAULT_SPEED, angle, 0, previous_time, previous_error, current_integral, KP, KI, KD)
 
-            left_motor, right_motor = pid_motor_speed(DEFAULT_SPEED, angle, 0, KP)
-            print('angle', left_motor, right_motor)
-            mc.change_speed(left_motor, right_motor)
+        #     left_motor, right_motor = pid_motor_speed(DEFAULT_SPEED, angle, 0, KP)
+        #     print('angle', left_motor, right_motor)
+        #     mc.change_speed(left_motor, right_motor)
 
         # elif (arrow != None):
         #     arrow = arrow * ARROW_MULTIPLIER
@@ -175,7 +175,7 @@ try:
         #     print('arrow', left_motor, right_motor, arrow)
         #     mc.change_speed(left_motor, right_motor)
         
-        elif (finish_angle != None):
+        if (finish_angle != None):
             left_motor, right_motor, previous_time, previous_error, current_integral \
                 = pid_motor_speed(DEFAULT_SPEED, finish_angle, np.pi/2, previous_time, previous_error, current_integral, KP, KI, KD)
 
