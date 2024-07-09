2571edce68c3cdf0777666fce138afb76a9b6570
DilonH <dilonhewamanna@outlook.com>
2024-07-10 00:07:31 +1000

    Update arrow_detect_2.py

--- nationals/arrow_detect_2.py
+++ nationals/arrow_detect_2.py
@@ -1,25 +1,48 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
-capture = cv.VideoCapture(0)
-capture.set(3, 320)                         # Set Video Width to 320px
-capture.set(4, 240)                         # Set Video Height to 240px
+def preprocess(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
+    img_canny = cv2.Canny(img_blur, 50, 50)
+    kernel = np.ones((3, 3))
+    img_dilate = cv2.dilate(img_canny, kernel, iterations=2)
+    img_erode = cv2.erode(img_dilate, kernel, iterations=1)
+    return img_erode
 
+def find_tip(points, convex_hull):
+    length = len(points)
+    indices = np.setdiff1d(range(length), convex_hull)
+
+    for i in range(2):
+        j = indices[i] + 2
+        if j > length - 1:
+            j = length - j
+        if np.all(points[j] == points[indices[i - 1] - 2]):
+            return tuple(points[j])
+
+capture = cv2.VideoCapture(0)
+capture.set(3, 320)                         # Set Video Width to 320px
+capture.set(4, 240)                         
 
 while True:
-    ret, img = capture.read()
-    src = np.float32([[0, 240], [320, 240], [0, 0], [320, 0]])
-    dst = np.float32([[0, 240], [320, 240], [0, 0], [320, 0]])
-    M = cv.getPerspectiveTransform(src, dst) # The transformation matrix
-    Minv = cv.getPerspectiveTransform(dst, src) # Inverse transformation
-    # Apply np slicing for ROI crop
-    warped_img = cv.warpPerspective(img, M, (320, 240)) # Image warping
+    img = capture.read()
 
-    cv.imshow('frame', warped_img)
-    key = cv.waitKey(1)
+    contours, hierarchy = cv2.findContours(preprocess(img), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+    for cnt in contours:
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.025 * peri, True)
+        hull = cv2.convexHull(approx, returnPoints=False)
+        sides = len(hull)
+
+        if 6 > sides > 3 and sides + 2 == len(approx):
+            arrow_tip = find_tip(approx[:,0,:], hull.squeeze())
+            if arrow_tip:
+                cv2.drawContours(img, [cnt], -1, (0, 255, 0), 3)
+                cv2.circle(img, arrow_tip, 3, (0, 0, 255), cv2.FILLED)
+
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
     if key == ord('f'):
         break
-
-    elif key == ord('p'):
-        cv.waitKey(-1)
