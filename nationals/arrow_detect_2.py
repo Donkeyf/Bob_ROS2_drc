e62be4a8accc78d889a62b10a12764cdba539609
DilonH <dilonhewamanna@outlook.com>
2024-07-09 23:55:39 +1000

    Update arrow_detect_2.py

--- nationals/arrow_detect_2.py
+++ nationals/arrow_detect_2.py
@@ -8,12 +8,11 @@ capture.set(4, 240)                         # Set Video Height to 240px
 
 while True:
     ret, img = capture.read()
-    src = np.float32([[0, 240], [1207, 240], [0, 0], [320, 0]])
-    dst = np.float32([[569, 240], [711, 240], [0, 0], [320, 0]])
+    src = np.float32([[0, 240], [320, 240], [0, 0], [320, 0]])
+    dst = np.float32([[0, 240], [320, 240], [0, 0], [320, 0]])
     M = cv.getPerspectiveTransform(src, dst) # The transformation matrix
     Minv = cv.getPerspectiveTransform(dst, src) # Inverse transformation
-
-    img = img[450:(450+240), 0:320] # Apply np slicing for ROI crop
+    # Apply np slicing for ROI crop
     warped_img = cv.warpPerspective(img, M, (320, 240)) # Image warping
 
     cv.imshow('frame', warped_img)
